e659793e51a82c7841671e882109b48ba79d7db2
smah0010 <smah0010@student.monash.edu>
2020-10-16 01:21:55 +0800

    Can now zoom in and out with Ctrl+MouseWheel
    - Currently there is no way to reset the zoom, and I don't think I
    should do that since the grid is based on screen size.
    - The performance has wicked drops if the user zooms out too far, I'm
    also worried if it might affect larger screens harder. Might look into
    optimizations regarding the visible area in the future.

--- Canvas/InfiniteCanvas.py
+++ Canvas/InfiniteCanvas.py
@@ -16,8 +16,9 @@ class InfiniteCanvas(tk.Frame):
 
         # Frame children
         self.canvas = None
-        self.xscrollbar = None
-        self.yscrollbar = None
+        self.canvas_xscrollbar = None
+        self.canvas_yscrollbar = None
+        self.grid_size = 50
 
         # Frame data
         self.width = width
@@ -30,8 +31,13 @@ class InfiniteCanvas(tk.Frame):
         # Dragging variables
         self.drag_src_coords = (-1, -1)  # (x, y)
 
-        # Create canvas and children
+        # Grids
+        self.grid = []
+
+        # Create canvas
         self.create_canvas()
+        self.create_canvas_events()
+        self.create_canvas_grid()
 
     # ------------------------------------ Events --------------------------------------- #
     def __handle_button_motion(self, event):
@@ -70,6 +76,37 @@ class InfiniteCanvas(tk.Frame):
         """
         if self.mode == "drag":
             self.drag_src_coords = (-1, -1)
+            self.canvas.config(yscrollincrement=0, xscrollincrement=0)
+
+    def __handle_zoom(self, event):
+        if self.mode == "zoom":
+            scale_factor = 1.001 ** event.delta
+            self.canvas.scale(tk.ALL, event.x, event.y, scale_factor, scale_factor)
+            bbox = self.canvas.bbox(tk.ALL)
+            self.canvas.config(scrollregion=(bbox[0], bbox[1], bbox[2], bbox[3]))
+
+    # ------------------------- Mode Events ----------------------------- #
+    def __in_default_mode(self):
+        """
+        Checks if the mode is default.
+
+        :return: Boolean indicating if the mode is default. True if default, False otherwise.
+        """
+        return self.mode == ""
+
+    def __set_mode(self, mode):
+        """
+        Set the mode to some other mode
+
+        :param mode: The mode to set.
+        """
+        self.mode = mode
+
+    def __reset_mode(self):
+        """
+        Reset the mode to default.
+        """
+        self.mode = ""
 
     def __set_drag_mode(self, event):
         """
@@ -78,50 +115,93 @@ class InfiniteCanvas(tk.Frame):
         :param event: The tkinter event
         """
         # Check whether to activate or deactivate the drag event
-        if str(event.type) == "KeyPress" and self.mode != "drag":
-            self.mode = "drag"
+        if str(event.type) == "KeyPress" and self.__in_default_mode():
+            self.__set_mode("drag")
             self.canvas.config(cursor="fleur")
         elif str(event.type) == "KeyRelease":
             # Reset Drag variables
-            self.mode = ""
+            self.__reset_mode()
             self.drag_src_coords = (-1, -1)
             self.canvas.config(cursor="arrow")
+
+    def __set_zoom_mode(self, event):
+        """
+        Set Drag Mode. Used to enable and disable drag events.
+
+        :param event: The tkinter event
+        """
+        # Check whether to activate or deactivate the zoom event
+        if str(event.type) == "KeyPress" and self.__in_default_mode():
+            self.__set_mode("zoom")
+        elif str(event.type) == "KeyRelease":
+            # Reset Zoom variables
+            self.__reset_mode()
 
     # ----------------------------------- Canvas Method ------------------------------------ #
     def create_canvas(self):
         """
         Create and pack the canvas.
         """
-        # Create a canvas with double the width and double the height so that the user has more room to work with
-        cwidth = self.winfo_screenwidth()*2
-        cheight = self.winfo_screenheight()*2
-        self.canvas = tk.Canvas(master=self, width=cwidth, height=cheight, scrollregion=(0, 0, cwidth, cheight))
+        # Create a  canvas with double the width and double the height so that the user has more room to work with
+        cwidth = self.winfo_screenwidth() * 2
+        cheight = self.winfo_screenheight() * 2
+        self.canvas = tk.Canvas(master=self, width=cwidth, height=cheight,
+                                scrollregion=(0, 0, cwidth, cheight), confine=True)
         self.canvas.configure(bg="grey")    # To see if it is being rendered correctly
 
         # XScrollbar
-        self.xscrollbar = tk.Scrollbar(master=self, orient=tk.HORIZONTAL)
-        self.xscrollbar.config(command=self.canvas.xview)
+        self.canvas_xscrollbar = tk.Scrollbar(master=self, orient=tk.HORIZONTAL)
+        self.canvas_xscrollbar.config(command=self.canvas.xview)
 
         # YScrollbar
-        self.yscrollbar = tk.Scrollbar(master=self, orient=tk.VERTICAL)
-        self.yscrollbar.config(command=self.canvas.yview)
+        self.canvas_yscrollbar = tk.Scrollbar(master=self, orient=tk.VERTICAL)
+        self.canvas_yscrollbar.config(command=self.canvas.yview)
 
         # Pack
-        self.xscrollbar.pack(side=tk.BOTTOM, fill=tk.X)
-        self.yscrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.canvas_xscrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.canvas_yscrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
         # Pack Canvas
-        self.canvas.config(xscrollcommand=self.xscrollbar.set, yscrollcommand=self.yscrollbar.set)
+        self.canvas.config(xscrollcommand=self.canvas_xscrollbar.set, yscrollcommand=self.canvas_yscrollbar.set)
         self.canvas.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
         self.canvas.focus_set()
 
+    def create_canvas_events(self):
+        """
+        Create canvas events. Used to bind events to the canvas widget.
+        """
         # Create events for the canvas
+        # Mouse Drag and release
         self.canvas.bind("<B1-Motion>", self.__handle_button_motion)
         self.canvas.bind("<ButtonRelease-1>", self.__handle_button_release)
 
+        # Mouse Scroll
+        self.canvas.bind("<MouseWheel>", self.__handle_zoom)
+
+        # Drag mode
         self.canvas.bind("<KeyPress-space>", self.__set_drag_mode)
         self.canvas.bind("<KeyRelease-space>", self.__set_drag_mode)
 
+        # Zoom mode
+        self.canvas.bind("<KeyPress-Control_L>", self.__set_zoom_mode)
+        self.canvas.bind("<KeyRelease-Control_L>", self.__set_zoom_mode)
+
+    def create_canvas_grid(self):
+        """
+        Create canvas grid. Used to generate the grid on the canvas.
+        """
+        cwidth = self.winfo_screenwidth() * 2
+        cheight = self.winfo_screenheight() * 2
+
+        # Create the grid
+        for x1 in range(0, cwidth-self.grid_size, self.grid_size):
+            x2 = x1 + self.grid_size
+            for y1 in range(0, cheight-self.grid_size, self.grid_size):
+                y2 = y1 + self.grid_size
+                self.canvas.create_rectangle(x1, y1, x2, y2)
+
+        # Set scroll region within the confines of the grid
+        self.canvas.config(scrollregion=self.canvas.bbox(tk.ALL))
 
 
 
